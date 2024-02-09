26be179ca44e022a2a0155b38ef46de03a177e27
Akif Albayrak <akifalbayrak6161@gmail.com>
2024-02-09 21:22:29 +0300

    working on vue

--- basics-assignment-3-problem/app.js
+++ basics-assignment-3-problem/app.js
@@ -1,33 +1,34 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            counter: 0,
-        };
-    },
-    watch:{
-        result(){
-            console.log("Watcher executing...")
-           const that = this;
-           setTimeout(() => {
-            that.counter=0;
-           }, 2000); 
-        }
-    },
-    computed: {
-        result() {
-            if (this.counter < 37) {
-                return "Not there yet";
-            }else if(this.counter===37){
-                return this.counter
-            }else {
-                return "Too much!"
-            }
-        },
-    },
-    methods: {
-        addNumber(num) {
-            this.counter += num;
-        },
-    },
+  data() {
+    return {
+      number: 0
+    };
+  },
+  computed: {
+    result() {
+      if (this.number < 37) {
+        return 'Not there yet!';
+      } else if (this.number === 37) {
+        return this.number;
+      } else {
+        return 'Too much!';
+      }
+    }
+  },
+  watch: {
+    result() {
+      console.log('Watcher executing...');
+      const that = this;
+      setTimeout(function() {
+        that.number = 0;
+      }, 5000);
+    }
+  },
+  methods: {
+    addNumber(num) {
+      this.number = this.number + num;
+    }
+  }
 });
-app.mount("#assignment");
+
+app.mount('#assignment');
